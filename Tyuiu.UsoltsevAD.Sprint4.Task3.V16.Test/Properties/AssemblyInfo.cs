90d7d4035b43460a0bfbd437cd3094bc584677ba
Tyuiu.MalsagovUA@outlook.com <mals.umar@yandex.ru>
2024-05-26 18:07:05 +0500

    Task3

---
+++ Tyuiu.UsoltsevAD.Sprint4.Task3.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.UsoltsevAD.Sprint4.Task3.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.UsoltsevAD.Sprint4.Task3.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7216ba4-6a36-4c77-97c9-f4282e75bdfa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
