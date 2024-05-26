d9624fc692afff3f8208e21e6899c2e482f0d494
Tyuiu.MalsagovUA@outlook.com <mals.umar@yandex.ru>
2024-05-26 19:53:30 +0500

    Task7

---
+++ Tyuiu.UsoltsevAD.Sprint4.Task7.V8.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.UsoltsevAD.Sprint4.Task7.V8.Lib
+{
+    public class DataService : ISprint4Task7V8
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int res = 0;
+            foreach (char element in value)
+            {
+                if ((int)Char.GetNumericValue(element) % 2 == 1)
+                {
+                    res += (int)Char.GetNumericValue(element);
+                }
+            }
+            return res;
+        }
+    }
+}
